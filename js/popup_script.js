cfa1e5fec1b9ef0520511131166004ef78bf15d5
Malkey <lintucrow@yandex.ru>
2016-08-30 01:19:17 +0300

    Поправила некоторые косяки и дописала JS для формы

--- js/popup_script.js
+++ js/popup_script.js
@@ -1,8 +1,49 @@
 var btn = document.querySelector(".hotel-search-btn");
 var popup = document.querySelector(".hotel-search-popup");
+var form = popup.querySelector(".search-form");
+var search_btn = popup.querySelector(".search-btn");
+
+var arrival = popup.querySelector("[name=arrival-date]");
+var departure = popup.querySelector("[name=departure-date]");
+var adult = popup.querySelector("[name=adult]");
+var children = popup.querySelector("[name=children]");
+
+var storage_adult = localStorage.getItem("adult");
+var storage_children = localStorage.getItem("children");
 
 
 btn.addEventListener("click", function (event) {
   event.preventDefault();
   popup.classList.toggle("hotel-search-popup_show");
+
+  search_btn.classList.remove("search-btn_error");
+  search_btn.offsetWidth = popup.offsetWidth;
+
+  if (storage_adult) {
+    adult.value = storage_adult;
+  };
+
+  if (storage_children) {
+    children.value = storage_children;
+  };
+});
+
+form.addEventListener("submit", function (event) {
+  if (!arrival.value || !departure.value || !adult.value || !children.value) {
+    event.preventDefault();
+    search_btn.classList.remove("search-btn_error");
+    search_btn.offsetWidth = popup.offsetWidth;
+    search_btn.classList.add("search-btn_error");
+  } else {
+    localStorage.setItem("adult", adult.value);
+    localStorage.setItem("children", children.value);
+  }
+});
+
+window.addEventListener("keydown", function (event) {
+  if (event.keyCode === 27) { //Esc key
+    if (popup.classList.contains("hotel-search-popup_show")) {
+      popup.classList.remove("hotel-search-popup_show");
+    }
+  }
 });
